9902ffb578d48d43cdda9beaaae46b69408dc355
Shweta Patil <shweta.patil@lexisnexis.co.uk>
2020-05-29 14:45:33 +0100

    refactoring for dynamic variable

--- src/components/user/UserItem.js
+++ src/components/user/UserItem.js
@@ -8,19 +8,20 @@ class UserItem extends Component {
         html_url:'https://github.com/pjhyett'
     }
     render() {
+        const {login, avatar_url, html_url} = this.state
         return (
             <div className='card text-center'>
                 <img 
-                    src={this.state.avatar_url} 
+                    src={avatar_url} 
                     alt=''
                     className='round-img'
                     style={{ width: '60px' }} 
                 />
 
-                <h3>{this.state.login}</h3>
+                <h3>{login}</h3>
 
                 <div>
-                    <a href={this.state.html_url} className='btn btn-dark btn-sm my-1'>more</a>
+                    <a href={html_url} className='btn btn-dark btn-sm my-1'>more</a>
                 </div>
             </div>
         )
